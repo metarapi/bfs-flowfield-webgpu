8734ef72356446a4a414294ce10738b05448517c
metarapi <dino.metarapi@gmail.com>
2025-06-24 01:00:07 +0200

    Fix CSS imports and dependencies for deployment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import 'flyonui/flyonui.js';
+// import 'flyonui/flyonui.js';
 import { PathfindingSimulation } from './js/PathfindingSimulation.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
